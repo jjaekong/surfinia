c7093285024819212f0f33de9918adaa87225cbe
kyh5392 <kyh90495392@gmail.com>
2016-07-28 16:26:57 +0900

    커뮤니티 > 글쓰기 (form) 태그 수정 #2

--- community/event_write.php
+++ community/event_write.php
@@ -72,17 +72,19 @@
                 <div class="comment">
                     <h3>COMMENT</h3>
                     <div class="comment-write">
-                        <label for="user-name">이름</label>
-                        <input type="text" id="user-name">
-                        <label for="user-pw">패스워드</label>
-                        <input type="password" id="user-pw">
-                        <label for="comment-content" class="sr-only"></label>
-                        <textarea id="comment-content"></textarea>
-                        <div class="btn-area">
-                            <p>
-                                <a href="#" class="btn btn-green" role="button">글쓰기</a>
-                            </p>
-                        </div>
+						<form>
+							<label for="user-name">이름</label>
+							<input type="text" id="user-name">
+							<label for="user-pw">패스워드</label>
+							<input type="password" id="user-pw">
+							<label for="comment-content" class="sr-only"></label>
+							<textarea id="comment-content"></textarea>
+							<div class="btn-area">
+								<p>
+									<a href="#" class="btn btn-green" role="button">글쓰기</a>
+								</p>
+							</div>
+						</form>
                     </div>
                     <ul class="comment-view">
                         <li>
--- community/qna_write.php
+++ community/qna_write.php
@@ -11,30 +11,32 @@
                     <h1>Q&amp;A</h1>
                 </div>
                 <div class="table-wrap">
-                    <table class="table table-border">
-                        <colgroup>
-                            <col style=" width: 159px;">
-                            <col style=" width: 1011px;">
-                        </colgroup>
-                        <tbody>
-                            <tr>
-                                <th><label for="subject">제목</label></th>
-                                <td><input type="text" id="subject"></td>
-                            </tr>
-                            <tr>
-                                <th><label for="product-review">상품평</label></th>
-                                <td><textarea id="product-review" ></textarea></td>
-                            </tr>
-                            <tr>
-                                <th><label for="photo-file"></label>사진첨부</th>
-                                <td><input type="file" id="photo-file"></td>
-                            </tr>
-                            <tr>
-                                <th><label for="user-pw"></label>비밀번호</th>
-                                <td><input type="password" id="user-pw"></td>
-                            </tr>
-                        </tbody>
-                    </table>
+					<form>
+						<table class="table table-border">
+							<colgroup>
+								<col style=" width: 159px;">
+								<col style=" width: 1011px;">
+							</colgroup>
+							<tbody>
+								<tr>
+									<th><label for="subject">제목</label></th>
+									<td><input type="text" id="subject"></td>
+								</tr>
+								<tr>
+									<th><label for="product-review">상품평</label></th>
+									<td><textarea id="product-review" ></textarea></td>
+								</tr>
+								<tr>
+									<th><label for="photo-file"></label>사진첨부</th>
+									<td><input type="file" id="photo-file"></td>
+								</tr>
+								<tr>
+									<th><label for="user-pw"></label>비밀번호</th>
+									<td><input type="password" id="user-pw"></td>
+								</tr>
+							</tbody>
+						</table>
+					</form>
                     <div class="btn-area">
                         <p>
                             <a href="#" class="btn btn-green" role="button">등록</a>
