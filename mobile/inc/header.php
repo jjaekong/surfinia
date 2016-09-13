ab05b8d7cb554ca9a2f9b316981f79e55f71c662
kyh5392 <kyh90495392@gmail.com>
2016-09-13 10:17:26 +0900

    cummit test

--- mobile/inc/header.php
+++ mobile/inc/header.php
@@ -22,63 +22,61 @@
 <nav id="nav" class="collapsed" tabindex="-1">
     <div class="nav-backdrop"></div>
     <div class="nav-container">
+        <button type="button" class="btn btn-nav-close">
+            <img src="/mobile/images/btn_close.png" alt="네비게이션 닫기">
+        </button>
         <div class="nav-content">
             <div class="nav-head">
-                <h1><img src="/mobile/images/main/logo.jpg" alt="메인로고"></h1>
+                <h1><img src="/mobile/images/main/menu_logo.jpg" alt="메인로고"></h1>
                 <div class="quick-menu">
+                    <div class="search-form">
+                        <form>
+                            <input type="text" class="form-control" id="search" placeholder="NEW">
+                            <label for="search" class="sr-only">검색</label>
+                            <button class="btn"><span></span></button>
+                        </form>
+                    </div>
+                    <div class="btn-area">
+                        <p>
+                            <a href="#" class="btn btn-basics btn-gray" role="button">회원가입</a>
+                            <a href="#" class="btn btn-basics btn-charcoal" role="button">로그인</a>
+                        </p>
+                    </div>
                     <ul>
-                        <li><a href="/mobile/member/login.php">로그인</a></li>
-                        <li><a href="/mobile/member/join_step01.php">회원가입</a></li>
-                        <li><a href="#">마이페이지</a></li>
-                        <li><a href="/mobile/mypage/basket.php">장바구니(0)</a></li>
+                        <li>
+                            <a href="#">마이하트</a>
+                        </li>
+                        <li>
+                            <a href="#">배송조회</a>
+                        </li>
                     </ul>
                 </div>
             </div>
             <ul class="gnb">
                 <li>
-                    <a href="#">NEW<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">NEW</a>
                 </li>
                 <li>
-                    <a href="#">BLANKET<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">CUSHION</a>
                 </li>
                 <li>
-                    <a href="#">CUSHION<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">BLANKET</a>
                 </li>
                 <li>
-                    <a href="#">LIVING<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">LIVING<span></span></a>
+                    <ul>
+                        <li><a href="#">RUG&amp;MAT</a></li>
+                        <li><a href="#">SLIPPER</a></li>
+                    </ul
                 </li>
                 <li>
-                    <a href="#">ACCESSORY<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">ACCESSORY</a>
                 </li>
                 <li>
-                    <a href="#">SALE<span class="glyphicon glyphicon-menu-down"></span></a>
-                    <!--<ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                    </ul>-->
+                    <a href="#">SALE</a>
                 </li>
                 <li>
-                    <a href="#">COMMUNITY<span class="glyphicon glyphicon-menu-down"></span></a>
+                    <a href="#">COMMUNITY<span></span></a>
                     <ul>
                         <li><a href="#">공지사항</a></li>
                         <li><a href="#">Q&amp;A</a></li>
--- mobile/index.php
+++ mobile/index.php
@@ -11,6 +11,17 @@
                 <li><img src="/mobile/images/main/main_visual03.jpg" class="img-responsive" alt="메인 이미지"></li>
             </ul>
         </div>
+        <div class="event">
+            <div class="container">
+                <p class="event-img">
+                    <img src="/mobile/images/main/event_img01.jpg" class="img-responsive" alt="이벤트 이미지">
+                </p>
+                <h3>[서피니아 우리집 홈데코 스타일링] : 취향저격 홈스타일링 추천 &amp; 포토후기 이벤트!</h3>
+                <p>
+                    패브릭 전문 브랜드 서피니아의 베스트 상품을 무료로 받아보실 수 있습니다.포토후기를 작성해주신 분들 중 베스트 포토후기를 추첨하여 베스트 상품인 패치워크 울리블랭킷을 선물로 드립니다. 예쁜 블랭킷을 받을 수 있는 찬스! 이달의 리뷰왕이 되어보세요^^
+                </p>
+            </div>
+        </div>
         <div class="product-list">
             <div class="container">
                 <ul class="row">
@@ -69,48 +80,28 @@
                 </ul>
             </div>
         </div>
-        <div class="event">
-            <div class="container">
-                <p class="event-img">
-                    <img src="/mobile/images/main/event_img01.jpg" class="img-responsive" alt="이벤트 이미지">
-                </p>
-                <h3>[서피니아 우리집 홈데코 스타일링] : 취향저격 홈스타일링 추천 &amp; 포토후기 이벤트!</h3>
-                <p>
-                    패브릭 전문 브랜드 서피니아의 베스트 상품을 무료로 받아보실 수 있습니다.포토후기를 작성해주신 분들 중 베스트 포토후기를 추첨하여 베스트 상품인 패치워크 울리블랭킷을 선물로 드립니다. 예쁜 블랭킷을 받을 수 있는 찬스! 이달의 리뷰왕이 되어보세요^^
-                </p>
-                <div class="btn-area">
-                    <p>
-                        <a href="#" class="btn">MORE VIEW</a>
-                    </p>
-                </div>
-            </div>
-        </div>
         <div class="throw">
             <div class="container">
-                <div class="first">
-                    <div class="first-throw">
-                        <a href="#">
-                            <h4>페이크퍼 베어 쓰로우<small>FAKE BEAR THROW</small></h4>
-                        </a>
-                    </div>
-                    <div class="second-throw">
-                        <a href="#">
-                            <h4>페이크퍼 베어 쓰로우<small>FAKE BEAR THROW</small></h4>
-                        </a>
-                    </div>
-                </div>
-                <div class="last">
-                    <div class="third-throw">
-                        <a href="#">
-                            <h4>페이크퍼 베어 쓰로우<small>FAKE BEAR THROW</small></h4>
-                        </a>
-                    </div>
-                    <div  class="fourth-throw">
-                        <a href="#">
-                            <h4>페이크퍼 베어 쓰로우<small>FAKE BEAR THROW</small></h4>
-                        </a>
-                    </div>
-                </div>
+                <p>
+                    <a href="#">
+                        <img src="/mobile/images/main/throw_bg01.jpg" class="img-responsive" alt="페이크 베어 쓰로우 이미지">
+                    </a>
+                </p>
+                <p>
+                    <a href="#">
+                        <img src="/mobile/images/main/throw_bg02.jpg" class="img-responsive" alt="페이크 베어 쓰로우 이미지">
+                    </a>
+                </p>
+                <p>
+                    <a href="#">
+                        <img src="/mobile/images/main/throw_bg03.jpg" class="img-responsive" alt="페이크 베어 쓰로우 이미지">
+                    </a>
+                </p>
+                <p>
+                    <a href="#">
+                        <img src="/mobile/images/main/throw_bg04.jpg" class="img-responsive" alt="페이크 베어 쓰로우 이미지">
+                    </a>
+                </p>
             </div>
         </div>
         <div class="sponcership">
@@ -175,98 +166,6 @@
                                     </p>
                                 </a>
                             </div>
-                        </li>
-                        <li>
-                            <p class="review-img">
-                                <a href="#">
-                                    <img src="/mobile/images/main/review_img01.jpg" alt="review 이미지">
-                                </a>
-                            </p>
-                            <div class="detail">
-                                <a href="#">
-                                    <h4>포근하고 따뜻한 코요테 니트세트!</h4>
-                                    <p class="amount">120,000 won</p>
-                                    <p class="review-content">
-                                        서피니아의 코요테 니트 블랭킷! 페이크 느낌 전혀안나고 너무 포근해서 지금 날씨에 딱 좋을거 같아요~<br>
-                                        물빨래도 가능해서 세탁걱정도 따로 없고 완전만족!!
-                                    </p>
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="notice">
-            <div class="container">
-                <div class="notice-list">
-                    <h3>NOTICE</h3>
-                    <a href="#" class="more">MORE</a>
-                    <hr>
-                    <ul>
-                        <li>
-                            <a href="#">
-                                카드사 무이자 할부 이벤트 안내
-                                <span class="label">NEW</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                설 연휴 배송 안내 공지
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                홈테이블데코페어 2014 참가
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                서피니아, ‘별그대’ 인조모피 담요 추석선물
-                                서피니아, ‘별그대’ 인조모피 담요 추석선물
-                                서피니아, ‘별그대’ 인조모피 담요 추석선물
-                                서피니아, ‘별그대’ 인조모피 담요 추석선물
-                                서피니아, ‘별그대’ 인조모피 담요 추석선물
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                추석 연휴 배송 안내 공지
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="qna-list">
-                    <h3>Q&AMP;A</h3>
-                    <a href="#" class="more">MORE</a>
-                    <hr>
-                    <ul>
-                        <li>
-                            <a href="#">
-                                주문/입금 확인 문의 드려요
-                                <span class="label">Info</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                재입고문의요
-                                <span class="label">Info</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                배송관련 질문입니다
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                상품관련 궁금한 사항이 있어요
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                맞교환 신청합니다
-                            </a>
                         </li>
                     </ul>
                 </div>
